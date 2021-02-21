62a5bb45b1fd98957c67f1a252ab9e02ad1e29da
Maria Cecília <mariaceciliafelixbarbosa@gmail.com>
2021-02-21 15:16:18 -0300

    https://github.com/Ceciliafelixb/BlocodeNotas.git

---
+++ src/blocodenotasmariacecilia/BlocodeNotasMariaCecilia.java
@@ -0,0 +1,16 @@
+
+
+package blocodenotasmariacecilia;
+
+
+public class BlocodeNotasMariaCecilia {
+
+   
+    public static void main(String[] args) {
+        
+        GUI_BlocodeNotas gui_blocodenotas = new GUI_BlocodeNotas();
+        gui_blocodenotas.setVisible(true);
+        
+    }
+    
+}
